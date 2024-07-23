017f198c47322993178b1817dd89a090d3ff0684
Antonin-crypto <akamis53@gmail.com>
2024-07-23 11:24:54 +0200

    modified 9-try.js messge d'error

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,14 +1,13 @@
 export default function guardrail(mathFunction) {
   // Créer le tableau queue
   const queue = [];
-
   try {
     // Exécuter mathFunction et ajouter le résultat à fin du résultat
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
     // En cas d'erreur, ajouter le message d'erreur à la fin du résultat
-    queue.push(error.message);
+    queue.push(`${error.name}: ${error.message}`);
   } finally {
     // Ajouter le message final à la fin du résultat
     queue.push('Guardrail was processed');
