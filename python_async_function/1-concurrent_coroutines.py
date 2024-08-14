37b23a5f16082537281370ba087d096a89802c42
Antonin-crypto <akamis53@gmail.com>
2024-08-14 10:14:09 +0200

    modified fichier 1 update commentaire

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,11 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """sumary_line
-
-    Keyword arguments:
-    argument -- description
-    Return: return_description
+    """
+    Async routine that takes in 2 int arguments: n and max_delay
     """
     delay_n = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
     return sorted(delay_n)
