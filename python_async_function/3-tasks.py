baaaeb9b5abe458d40123345ae4a5932a2c1cc16
Antonin-crypto <akamis53@gmail.com>
2024-08-14 10:16:34 +0200

    modified fichier 3 update commentaire

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -7,13 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """
-    Takes an integer max_delay and returns an asyncio.Task.
-
-    Args:
-    max_delay (int): Maximum delay for the wait_random coroutine.
-
-    Returns:
-    asyncio.Task: An asyncio task wrapping the wait_random coroutine.
-    """
+    """Returns a asyncio.Task"""
     return asyncio.create_task(wait_random(max_delay))
