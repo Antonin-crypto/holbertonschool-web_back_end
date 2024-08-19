5cbae7702c5409f31fc6893cadf24d367b66a658
Antonin-crypto <akamis53@gmail.com>
2024-08-19 10:07:45 +0200

    modified the fiche 0-async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,12 +3,12 @@
 
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """The coroutine will loop 10 times,
       each time asynchronously wait 1 second"""
 
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
