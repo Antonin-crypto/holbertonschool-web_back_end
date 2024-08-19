c1818dfbfb28fcd2c2b69defe330ed6b32287c59
Antonin-crypto <akamis53@gmail.com>
2024-08-19 13:04:33 +0200

    modified 2-hypermedia

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -47,28 +47,22 @@ class Server:
         """
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
-        range = index_range(page, page_size)
+        start_index, end_index = index_range(page, page_size)
         pages: List[List] = self.dataset()
-        return pages[range[0]:range[1]]
-
+        return pages[start_index:end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """Returns a dictionary containing key-value pairs
         """
         dataset = self.dataset()
-        data: List[List] = self.get_page(page, page_size)
-        next_page = page + 1
-        if next_page * page_size > len(dataset):
-            next_page = None
-        prev_page = page - 1
-        if prev_page == 0:
-            prev_page = None
         total_pages: int = math.ceil(len(dataset) / page_size)
-        if data == []:
-            page_size = 0
+        data: List[List] = self.get_page(page, page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
 
         params: Dict[str, Any] = {
-            'page_size': page_size,
+            'page_size': len(data),
             'page': page,
             'data': data,
             'next_page': next_page,
