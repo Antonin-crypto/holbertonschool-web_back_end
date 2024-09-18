1a4e4ba7106e4120f1110a950d3981ba8f915d0a
Antonin-crypto <akamis53@gmail.com>
2024-09-18 09:16:13 +0200

    update task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,15 @@
-const readline = require('readline');
+const process = require('process');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
+
+
+  console.log('This important software is now closing');
+  process.exit();
 });
-
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.close();
-});
-
-
-rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
-});
-
-module.exports = rl;
