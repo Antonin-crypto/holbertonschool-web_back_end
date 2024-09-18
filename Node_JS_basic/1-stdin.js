6ec435135e8c0fb1e4762f92228e1024398b4524
Antonin-crypto <akamis53@gmail.com>
2024-09-18 09:22:08 +0200

    update task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,12 +4,15 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
-
+// Écouter les données entrantes
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
 
 
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+
+    console.log('This important software is now closing');
+  }
+
   process.exit();
-});
