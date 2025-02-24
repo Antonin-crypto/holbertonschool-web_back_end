c4ffc58d8cacb72dcdfb9479ddf149fc4f2527a5
Antonin-crypto <akamis53@gmail.com>
2025-02-24 14:05:34 +0100

    User profile

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -90,5 +90,22 @@ def logout() -> str:
         abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """Profile method to get the user profile"""
+    session_id = request.cookies.get('session_id')
+
+    if not session_id:
+        abort(403)
+
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        if not user:
+            raise ValueError("User not found")
+        return jsonify({"email": user.email}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
