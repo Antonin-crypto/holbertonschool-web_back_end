f54076cb2f51a6b80a4015d757944cc4fcffe058
Antonin-crypto <akamis53@gmail.com>
2025-02-24 13:13:09 +0100

    Get session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -55,3 +55,18 @@ class Auth:
 
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session ID for the user corresponding to the given email.
+        Returns:
+            str: The generated session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+            session_id = str(uuid.uuid4())
+
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
