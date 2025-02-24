fbe5f4477eb6508784e6319917aed5e99fdb6a2c
Antonin-crypto <akamis53@gmail.com>
2025-02-24 11:22:33 +0100

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Flask app
 """
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 # Create the Flask application
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -13,6 +14,22 @@ def welcome():
     """ Welcome methode
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Register a new user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "Missing email or password"}), 400
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 # Run the Flask application if this script is run directly
