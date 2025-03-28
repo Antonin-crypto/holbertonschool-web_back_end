50b4486a956360300cdb9cc08b7d8e8d3012fc7b
Antonin-crypto <akamis53@gmail.com>
2025-03-28 10:02:39 +0100

    stubs

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,21 +1,39 @@
-const sendPaymentRequestToApi = require("./4-payment");
 const sinon = require("sinon");
+const chai = require("chai");
+const expect = chai.expect;
 const Utils = require("./utils");
-const expect = require("chai").expect;
+const sendPaymentRequestToApi = require("./4-payment");
 
-describe("sendPaymentRequestToApi", function () {
-  it("should use Utils.calculateNumber with the correct arguments", function () {
-    const stub = sinon.stub(Utils, "calculateNumber").returns(10);
+describe("sendPaymentRequestToApi", () => {
+  let calculateNumberStub;
+  let consoleSpy;
 
-    const consoleSpy = sinon.spy(console, "log");
+  beforeEach(() => {
+    // Stub Utils.calculateNumber to always return 10
+    calculateNumberStub = sinon.stub(Utils, "calculateNumber").returns(10);
+    // Spy on console.log
+    consoleSpy = sinon.spy(console, "log");
+  });
 
+  afterEach(() => {
+    // Restore the stub and spy after each test
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+
+  it("should call Utils.calculateNumber with SUM, 100, 20", () => {
     sendPaymentRequestToApi(100, 20);
 
-    expect(stub.calledOnce).to.be.true;
-    expect(stub.calledWithExactly("SUM", 100, 20)).to.be.true;
-    expect(consoleSpy.calledWithExactly("The total is: " + 10)).to.be.true;
+    // Verify the stub was called correctly
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.calledWith("SUM", 100, 20)).to.be.true;
+  });
 
-    stub.restore();
-    consoleSpy.restore();
+  it("should log the correct total to console", () => {
+    sendPaymentRequestToApi(100, 20);
+
+    // Verify console output
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 10")).to.be.true;
   });
 });
