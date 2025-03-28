6a4f5a4512c361acbdca3101a94b6a35d25ab67f
Antonin-crypto <akamis53@gmail.com>
2025-03-28 10:08:10 +0100

    Hooks

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,30 +1,30 @@
-const sendPaymentRequestToApi = require("./5-payment");
 const sinon = require("sinon");
-const Utils = require("./utils");
-const expect = require("chai").expect;
+const chai = require("chai");
+const expect = chai.expect;
+const sendPaymentRequestToApi = require("./5-payment");
 
-describe("sendPaymentRequestToApi", function () {
+describe("sendPaymentRequestToApi", () => {
   let consoleSpy;
 
   beforeEach(() => {
+    // Runs before each test
     consoleSpy = sinon.spy(console, "log");
   });
 
   afterEach(() => {
+    // Runs after each test
     consoleSpy.restore();
   });
 
-  it('should log "The total is: 120" and be called once when called with 100 and 20', function () {
+  it('should log "The total is: 120" and called once for (100, 20)', () => {
     sendPaymentRequestToApi(100, 20);
-
     expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWithExactly("The total is: 120")).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 120")).to.be.true;
   });
 
-  it('should log "The total is: 20" and be called once when called with 10 and 10', function () {
+  it('should log "The total is: 20" and called once for (10, 10)', () => {
     sendPaymentRequestToApi(10, 10);
-
     expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWithExactly("The total is: 20")).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 20")).to.be.true;
   });
 });
